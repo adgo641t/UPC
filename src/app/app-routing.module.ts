d576fb70e75a72ee75372417b8e1ec47c7ab13a3
Alex M.R <alexmillonesr@gmail.com>
2023-02-07 16:12:02 +0100

    Routed RankingComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ErrorPageComponent } from './views/error-page/error-page.component';
 import { HomeComponent } from './views/home/home.component';
 import { LoginComponent } from './views/login/login.component';
+import { RankingComponent } from './views/ranking/ranking.component';
 import { RegisterComponent } from './views/register/register.component';
 import { ShowGameComponent } from './views/show-game/show-game.component';
 
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'Login', component: LoginComponent },
   { path: 'Register', component: RegisterComponent },
   { path: 'Showgame', component: ShowGameComponent },
+  { path: 'Ranking', component: RankingComponent },
 
   { path: '', redirectTo: 'Home', pathMatch: 'full'},
   { path: '**', component: ErrorPageComponent}
