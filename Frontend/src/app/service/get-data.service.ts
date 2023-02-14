a2e71c3496f78ee32c6b0080f7b28934a5ebb039
adgo2021 <bl4ckgmz@gmail.com>
2023-02-14 13:09:28 +0100

    Access Token

---
+++ Frontend/src/app/service/get-data.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService  {
+
+  constructor(private http:HttpClient) { }
+
+
+  sendLogin(User: User): Observable<any> {
+    return this.http.post('http://localhost:3001/login',User);
+  }
+
+  sendRegister(User: User): Observable<any>{
+    console.log("dentro el servicio");
+    return this.http.post('http://localhost:3001/register',User);
+  }
+}
